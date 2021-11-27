626b973b15dcab7e1dda9c049fe48c9ddf999547
cuichenx <bob.cui@mail.utoronto.ca>
2021-11-27 18:36:42 -0500

    add conversion from mturk processed data to winogrande input

---
+++ scripts/preproc_to_winogrande_format.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from tqdm import tqdm
+
+def proc(sent):
+    if not sent.endswith("."):  # finish with period
+        sent += '.'
+    if not sent[0].isupper():  # start with a capital letter
+        sent = sent[0].upper() + sent[1:]
+    return sent
+
+
+def preproc_to_winogrande_format(df, out_path):
+    out_lines = []
+    line_format = '"qID": "{qID}", "sentence": "{sentence}", "option1": "{option1}", "option2": "{option2}", "answer": "{answer}"'
+    for i, line in tqdm(df.iterrows(), total=df.shape[0]):
+        s = proc(line['startphrase']) + ' _'
+        o1 = proc(line['ending1'])
+        o2 = proc(line['ending2'])
+        out_lines.append(
+            '{' + line_format.format(qID=line.get('qid', i), sentence=s, option1=o1, option2=o2, answer=int(line['labels'])+1) + '}\n'
+        )
+    with open(out_path, 'w') as f:
+        f.writelines(out_lines)
+
+
+if __name__ == '__main__':
+    for split in ('train', 'dev', 'test'):
+        df = pd.read_csv(f"/home/cuichenx/Courses/11-711/A4/data/{split}.csv")
+        df = df[df.valid == 1]
+        if split == 'train':
+            out_lines = f"../data/metaphor_s_train.jsonl"
+        else:
+            out_lines = f"../data/metaphor_{split}.jsonl"
+        preproc_to_winogrande_format(df, out_lines)
